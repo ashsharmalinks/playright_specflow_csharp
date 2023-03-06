825a5137304573e2ffa3921493c6a87f0c42a3ab
Ash Sharma <ashsharma.links@gmail.com>
2023-03-06 11:16:55 +0000

    specflow csharp project

---
+++ Features/EappLogin.feature
@@ -0,0 +1,10 @@
+﻿Feature: EEApp login1
+
+Scenario: login functionality for EEApp page
+	Given I have navigated to EEApp website
+	And I click on the login link
+	When I enter following login details
+	| UserName | Password |
+	| admin    | password |
+	Then I see the employee details
+	
---
+++ Steps/EapplginSteps.cs
@@ -0,0 +1,57 @@
+﻿using FluentAssertions;
+using SpecFlowProject_Playwright_Ash.Drivers;
+using SpecFlowProject_Playwright_Ash.Pages;
+using TechTalk.SpecFlow.Assist;
+
+namespace SpecFlowProject_Playwright_Ash.Steps;
+
+[Binding]
+public sealed class EapplginSteps
+{
+    // For additional details on SpecFlow step definitions see https://go.specflow.org/doc-stepdef
+
+    private readonly Driver _driver;
+    private readonly LoginPage _loginPage;
+    public EapplginSteps(Driver driver)
+    {
+        _driver = driver;
+        _loginPage = new LoginPage(_driver.Page);
+    }
+
+    [Given(@"I have navigated to EEApp website")]
+    public void GivenIHaveNavigatedToEeAppWebsite()
+    {
+        _driver.Page.GotoAsync("http://eaapp.somee.com/");
+    }
+    
+    [Then(@"I see the employee details")]
+    public async Task ThenISeeTheEmployeeDetails()
+    {
+      var isExist =  await  _loginPage.IsEmployeeDetails();
+      isExist.Should().Be(true);
+    }
+
+    [When(@"I enter the username and password")]
+    public void WhenIEnterTheUsernameAndPassword()
+    {
+    }
+
+    [Given(@"I click on the login link")]
+    public async Task GivenClickButton()
+    {
+        await _loginPage.ClickLogin();
+    }
+
+    [When(@"I enter the ""(.*)"" and ""(.*)""")]
+    public async  Task WhenIEnterTheAnd(string admin, string password)
+    {
+        await _loginPage.Login(admin, password);
+    }
+
+    [When(@"I enter following login details")]
+    public async Task WhenIEnterFollowingLoginDetails(Table table)
+    {
+        dynamic data = table.CreateDynamicInstance();
+        await _loginPage.Login((string)data.UserName, (string)data.Password);
+    }
+}
